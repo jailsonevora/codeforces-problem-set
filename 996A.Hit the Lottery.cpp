558faee913cf420763f0fcfa93ca451df6b3870c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-05 10:56:17 -0100

    996A

--- 996A.Hit the Lottery.cpp
+++ 996A.Hit the Lottery.cpp
@@ -3,3 +3,19 @@
 #include<vector>
 
 using namespace std;
+
+int bottomUp(int n){
+
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int n; 
+    cin>> n;
+
+    return bottomUp(n);
+}
