eda4c57503843b5db0308a6ef4e7c1b9e41adecd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-06 18:59:40 -0100

    1462

---
+++ 1462A.Favorite Sequence.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+vector<int> favoriteSequence(){
+
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    long long n; 
+    cin>> n;
+
+}
