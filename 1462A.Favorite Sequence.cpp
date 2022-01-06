814a04d2f73c3fcee792e9a7fc030cbd0067cb40
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-01-06 19:55:04 -0100

    1462A

--- 1462A.Favorite Sequence.cpp
+++ 1462A.Favorite Sequence.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include <sstream>
 
 using namespace std;
 
 vector<int> favoriteSequence(vector<int> sequence){
 
     vector<int> ans;
-    int pointer1 = sequence[0], pointer2 = sequence[1];
+    int pointer1 = 0, pointer2 = sequence.size()-1;
     
-    while(pointer1 != pointer2){
+    while(pointer1 <= pointer2){
 
-        ans.push_back();
+        if(pointer1 == pointer2){
+            ans.push_back(sequence[pointer1]); 
+            break;
+        }
+        ans.push_back(sequence[pointer1]);
+        ans.push_back(sequence[pointer2]);
+        ++pointer1;
+        --pointer2;
     }
-
+    return ans;
 }
 
 int main(){
@@ -21,10 +29,25 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
- 
-    long long n; 
-    cin>> n;
 
-    vector<int> sequence = {3, 4, 5, 2, 9, 1, 1};
+    int t;
+    while(cin>>t){
+        int n;
+        cin>>n;
 
+        int temp;
+        string line;
+        vector<int> sequence(n);
+
+        getline(std::cin, line);
+        istringstream ss(line);
+
+        while (ss >> temp)
+            sequence.push_back(temp);
+            
+        for(auto it: favoriteSequence(sequence))
+            cout<<it<<" "<<
+        endl;      
+    }
+    return 0;
 }
