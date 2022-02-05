4e79ed6d9dae5e08313f1310dcfc8ac48abc69cd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-05 00:11:11 -0100

    1519B tabulation

--- 1519B.The Cake Is a Lie.cpp
+++ 1519B.The Cake Is a Lie.cpp
@@ -13,18 +13,17 @@ void bottomUp(int n, int m, int k){
     std::fill(dp[0],dp[n],0);
 
     for (int row = 0; row < n; row++)
-        for (int col = 0; col < m; col++){
-            if(row==0 && col==0) 
-                continue;
-            if(row == 0)
-                dp[row][col] = dp[row][col-1] + 1;
-            else if(col == 0)
-                dp[row][col] = dp[row-1][col] + 1;
-            else
-                dp[row][col] = dp[row-1][col] + (col+1);
-        }
-        
-    (dp[n-1][m-1] == k) ? cout << "Yes" : cout << "No";    
+        dp[row][0] = row;
+
+    for (int col = 1; col < m; col++)
+        dp[0][col] = col;
+
+    for (int row = 1; row < n; row++)
+        for (int col = 1; col < m; col++)
+            dp[row][col] = dp[row-1][col] + (col+1);
+
+    (dp[n-1][m-1] == k) ? cout << "Yes" : cout << "No"; 
+    cout << endl; 
 }
 
 int main(){
@@ -37,18 +36,10 @@ int main(){
     cin>>t;
     for(int i=1; i <= t; ++i){
 
-        std::cin.ignore();
-        int temp;
-        string line;
-        vector<int> items;
-
-        getline(std::cin, line);
-        istringstream ss(line);
-
-        while (ss >> temp)
-            items.push_back(temp);
+        int n, m, k;
+        cin>>n>>m>>k;
         
-        bottomUp(items[0],items[1],items[2]);
+        bottomUp(n,m,k);
         std::cin.clear();
     }
     return 0;
