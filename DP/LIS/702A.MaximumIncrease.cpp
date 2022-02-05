96eed28eea35408dbf87e582551801135c156345
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-05 00:55:11 -0100

    702

--- DP/LIS/702A.MaximumIncrease.cpp
+++ DP/LIS/702A.MaximumIncrease.cpp
@@ -1,1 +1,49 @@
 // DP Longest Increassing Subsequence or Greedy Problem
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<sstream>
+#include<numeric>
+
+using namespace std;
+
+int greedy(vector<int> items){
+
+    int max = 0;
+    for (int i = 1; i < items.size(); i++)
+    {
+        if(items[i]>items[i-1])
+            max++;
+        else
+            max = 0;
+
+        if(items[i]>items[i-1] && i == items.size()-1)
+            max++;
+    }
+    return max;
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin>>n;
+    std::cin.ignore();
+
+    int temp;
+    string line;
+    vector<int> items;
+
+    getline(std::cin, line);
+    istringstream ss(line);
+
+    while (ss >> temp)
+        items.push_back(temp);
+
+    cout << greedy(items) << endl;
+    std::cin.clear();
+    return 0;
+}
