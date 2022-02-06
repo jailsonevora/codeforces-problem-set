536b959b00ffa2ff829a1463cdba57f943833e3f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-06 22:20:28 -0100

    500A

---
+++ Graph/500A.NewYearTransportation.cpp
@@ -0,0 +1,36 @@
+// DP Graph Problem
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<sstream>
+#include<numeric>
+
+using namespace std;
+
+int graph(int n, int v, vector<int> a){
+
+}
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n,v;
+    cin>>n>>v;
+    std::cin.ignore();
+
+    int temp;
+    string line;
+    vector<int> a;
+
+    getline(std::cin, line);
+    istringstream ss(line);
+
+    while (ss >> temp)
+        a.push_back(temp);
+
+}
