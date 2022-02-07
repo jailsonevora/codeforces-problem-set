40cff67f32d8f999ce873577ccfec2b5225e7e4f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-02-07 12:33:10 -0100

    894A

--- DP/LCS/894A.QAQ.cpp
+++ DP/LCS/894A.QAQ.cpp
@@ -1,1 +1,46 @@
 // DP Longest Common Subsequence Problem Pattern Matching
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<sstream>
+#include<numeric>
+
+using namespace std;
+
+int lcs(string s, string p){
+
+    int sz = s.size(), pz = p.size();
+
+    int dp[sz+1][pz+1];
+    int qaq = 0;
+
+    for (int row = 0; row <= sz; row++)
+        dp[row][0] = 0;
+    for (int col = 1; col <= pz; col++)
+        dp[0][col] = 0;
+
+    for (int row = 1; row <= sz; row++)
+        for (int col = 1; col <= pz; col++){
+            if(s[row-1] == p[col-1])
+                dp[row][col] = dp[row-1][col-1] + 1;
+            else
+                dp[row][col] = max(dp[row-1][col],dp[row][col-1]);
+
+            if(dp[row][col] == pz)
+                qaq++;
+        }
+    return qaq/pz;
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s,p;
+    cin>>s;
+    
+    p = "QAQ";
+    cout << lcs(s,p);
+}
