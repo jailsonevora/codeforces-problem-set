1dbf869a34c1531dc74b722eafc3f93fb9c7f2f9
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-04-08 19:37:07 -0100

    trie

--- Contacts.cpp
+++ Contacts.cpp
@@ -14,52 +14,59 @@ vector<string> split(const string &);
  */
 
 struct TrieNode
-{
-    int count;
-    TrieNode* children[26];
-
-    TrieNode()
     {
-        count = 0;
-        for(int i = 0; i < 26; ++i)
-            children[i] = nullptr;
+        int count;
+        TrieNode* children[26];
+
+        TrieNode()
+        {
+            count = 0;
+            for(int i = 0; i < 26; ++i)
+                children[i] = nullptr;
+        }
+    };
+
+class Solution{
+    
+    public:
+
+    TrieNode* root;
+
+    void insert(string word, TrieNode* root) {
+            
+        TrieNode *curr = root;
+
+        for (char c: word)
+        {
+            int index = c-'a';
+            if(!curr->children[index])
+                curr->children[index] = new TrieNode();
+            curr->count++;
+            
+            curr = curr->children[index];
+
+        }
+    }
+
+    int startsWith(string prefix, TrieNode* root) {
+
+        TrieNode *curr = root;
+
+        for (int i = 0; prefix[i] != '\0'; i++)
+        {
+            if(!curr->children[ prefix[i]-'a'])
+                return 0;
+            curr = curr->children[prefix[i]-'a'];            
+        }
+        return curr->count;        
     }
 };
-
-
-void insert(string word, TrieNode* root) {
-        
-    TrieNode *curr = root;
-
-    for (char c: word)
-    {
-        int index = c-'a';
-        if(!curr->children[index])
-            curr->children[index] = new TrieNode();
-        curr->count++;
-        
-        curr = curr->children[index];
-
-    }
-}
-
-int startsWith(string prefix, TrieNode* root) {
-
-    TrieNode *curr = root;
-
-    for (int i = 0; prefix[i] != '\0'; i++)
-    {
-        if(!curr->children[ prefix[i]-'a'])
-            return 0;
-        curr = curr->children[prefix[i]-'a'];            
-    }
-    return curr->count;        
-}
 
 vector<int> contacts(vector<vector<string>> queries) {
 
     vector<int> ans;
-    TrieNode* root = new TrieNode;
+    Solution sl;
+    sl.root = new TrieNode;
     for (int i = 0; i < queries.size(); i++){
         cout << queries[i][0];
         if(queries[i][0] == "add")
